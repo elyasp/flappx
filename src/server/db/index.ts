0aa57fdb224e385368df42d3fef134aa1a1981e1
Tetsujin <elyasp@outlook.com>
2020-09-14 03:13:19 +0200

    change db config

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,11 +2,11 @@ import * as mysql from "mysql";
 import config from "../config";
 import Bundles from "./bundles";
 
-export const Connection = mysql.createConnection(config.mysql);
+export const Connection = mysql.createPool(config.mysql);
 
-Connection.connect((error) => {
-  if (error) console.log(error);
-});
+// Connection.connect((error) => {
+//   if (error) console.log(error);
+// });
 
 export default {
   Bundles,
--- src/server/db/query/seed.sql
+++ src/server/db/query/seed.sql
@@ -1,6 +1,4 @@
-USE bundles;
-
-DROP TABLE package;
+USE heroku_94a7e72700e436c;
 
 CREATE TABLE `package` (
 	`name` varchar(45),
@@ -16,4 +14,6 @@ INSERT INTO `package` VALUES
 	("Tappx App","com.tappx.app","Tappx","info@tappx.es", TRUE, "Tool"), 
 	("Skyscanner App",'com.skyscanner.app',"Skyscanner","info@skyscanner.com", TRUE, 'Social'), 
 	("Angry Birds",'com.angrybirds.app',"Rovio","info@Rovio.es", FALSE, 'Games'), 
-	("Spotify",'net.spotify.app',"Tappx","info@tappx.es", TRUE, 'Music');
+	("Spotify",'net.spotify.app',"Tappx","info@tappx.es", TRUE, 'Music'),
+	("Test Image",'net.test.foto',"ACME","info@acme.es", FALSE, 'Social'),
+	("Example App",'com.example.app.EXA',"ExaCorp","contact@exacorp.es", TRUE, 'Tool');
